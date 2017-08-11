b31d98a186c6946ac07c0bf20eaf8a71049b09b5
ripfoghorn <igor.larcs@gmail.com>
2017-08-11 16:32:05 -0300

    fetching beer details

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import 'materialize-css'; // It installs the JS asset only
 import 'materialize-css/dist/css/materialize.min.css';
 import {Button} from 'react-materialize'
@@ -12,14 +11,13 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Brewmaster</h2>
         </div>
         <Button waves='light'>
             EDIT ME
         </Button>
         <CustomRouter/>
-        <BeerList/>
+         <BeerList/> 
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
